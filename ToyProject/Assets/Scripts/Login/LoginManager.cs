ba9d9c68930347db5e99d891f9e7e1e866e6923f
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2023-01-27 12:02:10 +0900

    0127 Desktop

---
+++ ToyProject/Assets/Scripts/Login/LoginManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class LoginManager : MonoBehaviour
+{
+    const string URL = "https://docs.google.com/spreadsheets/d/1r84Xh15BPtjMQk5m38sNB5co3IJSqDIKUCTtDHB8Nkc/export?format=tsv&range=A2:B";
+    
+    public void DoubleCheck()
+    {
+        //check Id
+
+        WWWForm form = new WWWForm();
+        form.AddField("order", "check");
+        //form.AddField("id",)
+    }
+    public void Login()
+    {
+
+    }
+    IEnumerator CPost(WWWForm form)
+    {
+
+    }
+    public void Regist()
+    {
+        
+    }
+}
