2e9977afc055e798a1b34337f933a5f6cb63ce0a
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2023-01-28 03:01:58 +0900

    20230127 DeskTop
    재수정

--- ToyProject/Assets/Scripts/Login/LoginManager.cs
+++ ToyProject/Assets/Scripts/Login/LoginManager.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 using UnityEngine.Networking;
 public class LoginManager : MonoBehaviour
 {
-    const string URL = "https://docs.google.com/spreadsheets/d/1r84Xh15BPtjMQk5m38sNB5co3IJSqDIKUCTtDHB8Nkc/export?format=tsv&range=A2:B";
+    public static LoginManager loginManager;
+    const string URL = "https://docs.google.com/spreadsheets/d/1r84Xh15BPtjMQk5m38sNB5co3IJSqDIKUCTtDHB8Nkc/export?format=tsv";
+
+    string id, password;    
     
-    public void DoubleCheck()
+    private void Awake()
     {
-        //check Id
-
-        WWWForm form = new WWWForm();
-        form.AddField("order", "check");
-        //form.AddField("id",)
-    }
-    public void Login()
+        if(loginManager == null)
+        {
+            loginManager = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }    
+   
+    public void Login(string _id,string _pw)
     {
-
+        id = _id;
+        password = _pw;
+            
     }
    
     public void Regist()
