8953f0b8f150dcaea58ad287cfdfd919feeeeab0
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2023-02-04 11:30:58 +0900

    0204DeskTop Login

--- ToyProject/Assets/Scripts/Login/LoginManager.cs
+++ ToyProject/Assets/Scripts/Login/LoginManager.cs
@@ -1,35 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
+
+[System.Serializable]
+class LoginMessage
+{
+    public string order { get; set; }
+    public string result { get; set; }
+    public string msg { get; set; }    
+}
 public class LoginManager : MonoBehaviour
 {
-    public static LoginManager loginManager;
-    const string URL = "https://docs.google.com/spreadsheets/d/1r84Xh15BPtjMQk5m38sNB5co3IJSqDIKUCTtDHB8Nkc/export?format=tsv";
+    string URL = "https://script.google.com/macros/s/AKfycbwqlDjWePQi1heqNZiyX3OJZMoqka_JhsTUrY7zzG0GWQp4U_wGduTK1D2nFZn6o4PP/exec";
 
-    string id, password;    
-    
-    private void Awake()
+    [SerializeField]
+    private InputField input_ID;
+    [SerializeField]
+    private InputField input_Pw;
+
+    private void Start()
     {
-        if(loginManager == null)
-        {
-            loginManager = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }    
-   
-    public void Login(string _id,string _pw)
-    {
-        id = _id;
-        password = _pw;
-            
+        WWWForm form = new WWWForm();
+        form.AddField("order", "login");
+        form.AddField("id", "ȫ�浿");
+        form.AddField("password", "123123");
+        StartCoroutine(CoPos(form));
     }
-   
+    public  void OnClickLogin()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("order", "login");
+        form.AddField("id", input_ID.text);
+        form.AddField("password", input_Pw.text);        
+    }
+    class UserData
+    {
+        public string id { get; }
+        public string password { get; }
+    }
+    IEnumerator CoPos(WWWForm form)
+    {   
+        using (UnityWebRequest www = UnityWebRequest.Post(URL,form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isDone)
+            {   
+                LoginMessage json = JsonUtility.FromJson<LoginMessage>(www.downloadHandler.text);
+                Debug.Log(json.order);
+                Debug.Log(json.result);
+            }
+            else
+            {
+                // errorMessage
+            }
+        }
+    }
+        
     public void Regist()
     {
-        
-    }
+
+    }   
 }
