1401348883073311922011fd3dcbb6fc5a403c3a
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2023-02-19 01:24:09 +0900

    0218 DeskTop Firebase

---
+++ ToyProject/Assets/Scripts/AuthManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using System;
+public class AuthManager 
+{
+    public static AuthManager instance;
+    public static AuthManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new AuthManager();
+            }           
+            return instance;
+        }
+    }
+    public FirebaseAuth Auth { get; private set; }
+    public FirebaseApp App { get; private set; }
+    public FirebaseUser User { get; private set; }
+    /// <summary>
+    /// Login : ID와 Password, 결과에대한 UI표시함수
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <param name="PW"></param>
+    /// <param name="UIAction"></param>
+    public void Login(string _id,string _pw,Action<string>_uiAction)
+    {
+        
+    }
+    /// <summary>
+    /// Regist : ID와 Password, 결과에대한 UI표시함수
+    /// </summary>
+    /// <param name="ID"></param>
+    /// <param name="PW"></param>
+    /// <param name="UIAction"></param>
+    public void Regist(string _id,string _pw,Action<string> _uiAction)
+    {
+
+    }
+
+
+}
