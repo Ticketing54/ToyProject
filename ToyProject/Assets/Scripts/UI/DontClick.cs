6c969e7663e8755c9eaa1fe710791c0665e23da8
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2023-03-21 13:03:24 +0900

    0320Desktop

--- ToyProject/Assets/Scripts/UI/DontClick.cs
+++ ToyProject/Assets/Scripts/UI/DontClick.cs
@@ -1,17 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class DontClick : MonoBehaviour
 {
     [SerializeField]
     ErrorMessage errorMessage;
+    [SerializeField]
+    TextMeshProUGUI text;
+
+    float count = 0;
+    float timer = 0;
     private void OnEnable()
     {
         if(errorMessage.gameObject.activeSelf == true)
         {
             errorMessage.gameObject.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >=1)
+        {
+            timer -= 1;
+            if(count==3)
+            {
+                text.text = "";
+                count = 0;
+            }
+            else
+            {
+                text.text += " .";
+                count++;
+            }
+            
         }
     }
     /// <summary>
